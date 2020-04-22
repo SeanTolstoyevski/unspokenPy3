6c273c63e135291d145b191106a08637780bee0b
SeanTolstoyevski <lostpars@outlook.com>
2020-04-21 21:26:20 -0700

    first commit

---
+++ addonSource/doc/en/README.md
@@ -0,0 +1,57 @@
+# NVDA Add-on Scons Template #
+
+This package contains a basic template structure for NVDA add-on development, building, distribution and localization.
+For details about NVDA add-on development please see the [NVDA Developer Guide](http://www.nvda-project.org/documentation/developerGuide.html).
+The NVDA addon development/discussion list [is here](http://www.freelists.org/list/nvda-addons)
+
+Copyright (C) 2012-2013 nvda addon team contributors.
+
+This package is distributed under the terms of the GNU General Public License, version 2 or later. Please see the file COPYING.txt for further details.
+
+## Features
+
+This template provides the following features you can use to help NVDA add-on development:
+
+*	Automatic add-on package creation, with naming and version loaded from a centralized build variables file (buildVars.py).
+*	Manifest file creation using a template (manifest.ini.tpl). Build variables are replaced on this template.
+*	Compilation of gettext mo files before distribution, when needed.
+- To generate a gettext pot file, please run scons pot. A **addon-name.pot** file will be created with all gettext messages for your add-on. You need to check the buildVars.i18nSources variable to comply with your requirements.
+*	Automatic generation of manifest localization files directly from gettext po files. Please make sure buildVars.py is included in i18nFiles.
+*	Automatic generation of HTML documents from markdown (.md) files, to manage documentation in different languages.
+
+## Requirements
+
+You need the following software to use this code for your NVDA add-ons development:
+
+- a Python distribution (2.7 or greater is recommended). Check the [Python Website](http://www.python.org) for Windows Installers.
+- Scons - [Website](http://www.scons.org/) - version 2.1.0 or greater. Install it using **easy_install** or grab an windows installer from the website.
+- GNU Gettext tools, if you want to have localization support for your add-on - Recommended. Any Linux distro or cygwin have those installed. You can find windows builds [here](http://gnuwin32.sourceforge.net/downlinks/gettext.php).
+- Markdown-2.0.1 or greater, if you want to convert documentation files to HTML documents. You can [Download Markdown-2.0.1 installer for Windows](https://pypi.python.org/pypi/Markdown/2.0.1).
+
+
+## Usage
+
+### To create a new NVDA add-on, taking advantage of this template: ###
+
+- Create an empty folder to hold the files for your add-on.
+- Copy the **addon** folder, the **buildVars.py** file, the manifest.ini.tpl file, the manifest-translated.ini.tpl, **SCONSTRUCT**, .gitignore and .gitattributes files to the created folder.
+- In the **buildVars.py** file, change variable **addon_info** with your add-on's information (name, summary, description, version, author and url).
+- Put your code in the usual folders for NVDA extension, under the **addon** folder. For instance: globalPlugins, synthDrivers, etc. You can delete folders you don't need for your particular add-on package.
+- Gettext translations must be placed into addon\locale\<lang>/LC_MESSAGES\nvda.po. 
+
+### To manage documentation files for your addon: ###
+
+- Copy the **readme.md** file for your addon, and the **docHandler.py** file, contained into addonTemplate, to the first created folder, where you copied **buildVars.py**. You can also copy **style.css** to improve the presentation of HTML documents.
+- Documentation files (named **readme.md**) must be placed into addon\doc\<lang>/.
+- Don't use **yourAddonName_docHandler.py** as a name for any file contained in **globalPlugins**; it will be removed to create a menu item for opening your addon documentation, under NVDA's help menu.
+
+Note: It's recommended to use hyphens instead of asterisks as tags for lists items, since they provide a better result when converting to HTML. You can also use asterisks followed by tab.
+
+### To package the add-on for distribution: ###
+
+- Open a command line, change to the folder that has the **SCONSTRUCT** file and run the **scons** command. The created add-on, if there were no errors, is placed in the current directory.
+- You can further customize variables in the **buildVars.py** file.
+
+Note that this template only provides a basic add-on structure and build infrastructure. You may need to adapt it for your specific needs.
+
+If you have any issues please use the NVDA addon list mensioned above.
---
+++ addonSource/globalPlugins/Unspoken/__init__.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import globalPluginHandler
+import NVDAObjects
+import config
+import speech
+import controlTypes
+from .camlorn_audio import *
+
+
+AUDIO_WIDTH = 10.0 # Width of the audio display.
+AUDIO_DEPTH = 5.0 # Distance of listener from display.
+
+UNSPOKEN_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+UNSPOKEN_SOUNDS_PATH = os.path.join(UNSPOKEN_ROOT_PATH, "sounds")
+
+sound_files={
+controlTypes.ROLE_CHECKBOX : "checkbox.wav",
+controlTypes.ROLE_RADIOBUTTON : "radiobutton.wav",
+controlTypes.ROLE_STATICTEXT : "editabletext.wav",
+controlTypes.ROLE_EDITABLETEXT : "editabletext.wav",
+controlTypes.ROLE_BUTTON : "button.wav",
+controlTypes.ROLE_MENUBAR : "menuitem.wav",
+controlTypes.ROLE_MENUITEM : "menuitem.wav",
+controlTypes.ROLE_MENU : "menuitem.wav",
+controlTypes.ROLE_COMBOBOX : "combobox.wav",
+controlTypes.ROLE_LISTITEM : "listitem.wav",
+controlTypes.ROLE_GRAPHIC : "icon.wav",
+controlTypes.ROLE_LINK : "link.wav",
+controlTypes.ROLE_TREEVIEWITEM : "treeviewitem.wav",
+controlTypes.ROLE_TAB : "tab.wav",
+controlTypes.ROLE_TABCONTROL : "tab.wav",
+controlTypes.ROLE_SLIDER : "slider.wav",
+controlTypes.ROLE_DROPDOWNBUTTON : "combobox.wav",
+controlTypes.ROLE_CLOCK: "clock.wav",
+controlTypes.ROLE_ANIMATION : "icon.wav",
+controlTypes.ROLE_ICON : "icon.wav",
+controlTypes.ROLE_IMAGEMAP : "icon.wav",
+controlTypes.ROLE_RADIOMENUITEM : "radiobutton.wav",
+controlTypes.ROLE_RICHEDIT : "editabletext.wav",
+controlTypes.ROLE_SHAPE : "icon.wav",
+controlTypes.ROLE_TEAROFFMENU : "menuitem.wav",
+controlTypes.ROLE_TOGGLEBUTTON : "checkbox.wav",
+controlTypes.ROLE_CHART : "icon.wav",
+controlTypes.ROLE_DIAGRAM : "icon.wav",
+controlTypes.ROLE_DIAL : "slider.wav",
+controlTypes.ROLE_DROPLIST : "combobox.wav",
+controlTypes.ROLE_MENUBUTTON : "button.wav",
+controlTypes.ROLE_DROPDOWNBUTTONGRID : "button.wav",
+controlTypes.ROLE_HOTKEYFIELD : "editabletext.wav",
+controlTypes.ROLE_INDICATOR : "icon.wav",
+controlTypes.ROLE_SPINBUTTON : "slider.wav",
+controlTypes.ROLE_TREEVIEWBUTTON: "button.wav",
+controlTypes.ROLE_DESKTOPICON : "icon.wav",
+controlTypes.ROLE_PASSWORDEDIT : "editabletext.wav",
+controlTypes.ROLE_CHECKMENUITEM : "checkbox.wav",
+controlTypes.ROLE_SPLITBUTTON : "splitbutton.wav",
+}
+
+sounds = dict() 
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def __init__(self, *args, **kwargs):
+		globalPluginHandler.GlobalPlugin.__init__(self, *args, **kwargs)
+
+		init_camlorn_audio()
+
+		for key in sound_files:
+			sounds[key] = Sound3D(os.path.join(UNSPOKEN_SOUNDS_PATH, sound_files[key]))
+			sounds[key].set_rolloff_factor(0)
+			if sounds[key].get_length() <= 0: pass
+			else: pass
+		self._room_reverb = Reverb()
+		self._room_reverb.set_reverb_density(0)
+		self._room_reverb.set_Decay_time(0.4)
+		self._room_reverb.set_gain(1)
+		self._room_reverb.set_reflections_gain(0.4)
+		self._room_reverb.set_late_reverb_gain(0)
+
+		self._NVDA_getSpeechTextForProperties = speech.getPropertiesSpeech
+		speech.getPropertiesSpeech = self._hook_getSpeechTextForProperties
+
+		self._previous_mouse_object = None
+
+
+	def _hook_getSpeechTextForProperties(self, reason=NVDAObjects.controlTypes.REASON_QUERY, *args, **kwargs):
+		role = kwargs.get('role', None)
+		if role:
+			if 'role' in kwargs and role in sounds:
+				del kwargs['role']
+		return self._NVDA_getSpeechTextForProperties(reason, *args, **kwargs)
+
+	def play_object(self, obj):
+		global AUDIO_WIDTH, AUDIO_DEPTH
+		role = obj.role
+		if role in sounds:
+			# Get coordinate bounds of desktop.
+			desktop = NVDAObjects.api.getDesktopObject()
+			desktop_max_x = desktop.location[2]
+			desktop_max_y = desktop.location[3]
+			desktop_aspect = float(desktop_max_y) / float(desktop_max_x)
+			# Get location of the object.
+			if obj.location != None:
+				# Object has a location. Get its center.
+				obj_x = obj.location[0] + (obj.location[2] / 2.0)
+				obj_y = obj.location[1] + (obj.location[3] / 2.0)
+			else:
+				# Objects without location are assumed in the center of the screen.
+				obj_x = desktop_max_x / 2.0
+				obj_y = desktop_max_y / 2.0
+			position_x = (obj_x / desktop_max_x) * (AUDIO_WIDTH * 2) - AUDIO_WIDTH
+			position_y = (obj_y / desktop_max_y) * (desktop_aspect * AUDIO_WIDTH * 2) - (desktop_aspect * AUDIO_WIDTH)
+			position_y *= -1
+			sounds[role].set_position(position_x, position_y, AUDIO_DEPTH * -1)
+			sounds[role].play()
+
+	def event_becomeNavigatorObject(self, obj, nextHandler):
+		self.play_object(obj)
+		nextHandler()
+
+	def event_mouseMove(self, obj, nextHandler, x, y):
+		if obj != self._previous_mouse_object:
+			self._previous_mouse_object = obj
+			self.play_object(obj)
+		nextHandler()
+
