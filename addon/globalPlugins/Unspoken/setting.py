920b6653277b10cbd36780ee64d8de2ec4b14966
adil s <ah.shaikh97@gmail.com>
2020-08-31 21:55:52 +0530

    refactor the code to add setting for sound theme

--- addon/globalPlugins/Unspoken/setting.py
+++ addon/globalPlugins/Unspoken/setting.py
@@ -1,26 +1,18 @@
-import os 
-import gui 
-import wx 
-
-UNSPOKEN_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-UNSPOKEN_SOUNDS_PATH = os.path.join(UNSPOKEN_ROOT_PATH, "sounds")
-
-def getAvailableSoundThemes():
-    soundThemes = []
-    dirs = os.listdir(UNSPOKEN_SOUNDS_PATH)
-    for d in dirs:
-        if os.path.isdir(os.path.join(UNSPOKEN_SOUNDS_PATH, d))
-        soundThemes.append(d)
-    return soundThemes
+import os
+import gui
+import wx
+from .soundtheme import *
+from logHandler import log
 
 
 class UnspokenSettingsPanel(gui.SettingsPanel):
     title = _(' unspoken  setting')
 
     def makeSettings(self, settingsSizer):
+        soundThemes = getAvailableSoundThemes()
         self.soundSelectorLbl = wx.StaticText(self, wx.ID_ANY, "sound theme")
-        self.SoundSelector = wx.Choice(self, wx.ID_ANY, choices=["default theme", "forest", "rain"])
+        self.SoundSelector = wx.Choice(self, wx.ID_ANY, choices=soundThemes)
         self.SoundSelector.SetSelection(0)
 
     def onSave(self):
-        pass
+        log.info("ok is pressed")
