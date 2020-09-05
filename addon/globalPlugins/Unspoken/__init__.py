ea31c5b597818932d0c51be564c5514cdc1e459b
SeanTolstoyevski <lostpars@outlook.com>
2020-09-06 00:39:28 +0300

    Revert "added settings to import and load new sound theme from zip file  (#2)"
    This reverts commit d588654c89ca50091d2b59459b7b42318f88473c.

--- addon/globalPlugins/Unspoken/__init__.py
+++ addon/globalPlugins/Unspoken/__init__.py
@@ -5,42 +5,77 @@
 
 import os
 # : NVDA's modules
-import addonHandler
-import config
-import controlTypes
-import globalPluginHandler
-import NVDAObjects
-import speech
-import ui
+import addonHandler, config, controlTypes, globalPluginHandler,  NVDAObjects, speech, ui
 # : 3rd party module
 from .camlorn_audio import *
-from .setting import UnspokenSettingsPanel
-import gui
-from .soundtheme import *
 
-AUDIO_WIDTH = 10.0  # Width of the audio display.
-AUDIO_DEPTH = 5.0  # Distance of listener from display.
+AUDIO_WIDTH = 10.0 # Width of the audio display.
+AUDIO_DEPTH = 5.0 # Distance of listener from display.
+
+UNSPOKEN_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+UNSPOKEN_SOUNDS_PATH = os.path.join(UNSPOKEN_ROOT_PATH, "sounds")
+
+sound_files = {
+controlTypes.ROLE_CHECKBOX : "checkbox.wav",
+controlTypes.ROLE_RADIOBUTTON : "radiobutton.wav",
+controlTypes.ROLE_STATICTEXT : "editabletext.wav",
+controlTypes.ROLE_EDITABLETEXT : "editabletext.wav",
+controlTypes.ROLE_BUTTON : "button.wav",
+controlTypes.ROLE_MENUBAR : "menuitem.wav",
+controlTypes.ROLE_MENUITEM : "menuitem.wav",
+controlTypes.ROLE_MENU : "menuitem.wav",
+controlTypes.ROLE_COMBOBOX : "combobox.wav",
+controlTypes.ROLE_LISTITEM : "listitem.wav",
+controlTypes.ROLE_GRAPHIC : "icon.wav",
+controlTypes.ROLE_LINK : "link.wav",
+controlTypes.ROLE_TREEVIEWITEM : "treeviewitem.wav",
+controlTypes.ROLE_TAB : "tab.wav",
+controlTypes.ROLE_TABCONTROL : "tab.wav",
+controlTypes.ROLE_SLIDER : "slider.wav",
+controlTypes.ROLE_DROPDOWNBUTTON : "combobox.wav",
+controlTypes.ROLE_CLOCK: "clock.wav",
+controlTypes.ROLE_ANIMATION : "icon.wav",
+controlTypes.ROLE_ICON : "icon.wav",
+controlTypes.ROLE_IMAGEMAP : "icon.wav",
+controlTypes.ROLE_RADIOMENUITEM : "radiobutton.wav",
+controlTypes.ROLE_RICHEDIT : "editabletext.wav",
+controlTypes.ROLE_SHAPE : "icon.wav",
+controlTypes.ROLE_TEAROFFMENU : "menuitem.wav",
+controlTypes.ROLE_TOGGLEBUTTON : "checkbox.wav",
+controlTypes.ROLE_CHART : "icon.wav",
+controlTypes.ROLE_DIAGRAM : "icon.wav",
+controlTypes.ROLE_DIAL : "slider.wav",
+controlTypes.ROLE_DROPLIST : "combobox.wav",
+controlTypes.ROLE_MENUBUTTON : "button.wav",
+controlTypes.ROLE_DROPDOWNBUTTONGRID : "button.wav",
+controlTypes.ROLE_HOTKEYFIELD : "editabletext.wav",
+controlTypes.ROLE_INDICATOR : "icon.wav",
+controlTypes.ROLE_SPINBUTTON : "slider.wav",
+controlTypes.ROLE_TREEVIEWBUTTON: "button.wav",
+controlTypes.ROLE_DESKTOPICON : "icon.wav",
+controlTypes.ROLE_PASSWORDEDIT : "editabletext.wav",
+controlTypes.ROLE_CHECKMENUITEM : "checkbox.wav",
+controlTypes.ROLE_SPLITBUTTON : "splitbutton.wav",
+}
+
+sounds = dict()
 
 confspec = {
-	"active": "boolean(default=true)",
-	"soundtheme": "default theme"
+	"active": "boolean(default=true)"
 }
 
 addonHandler.initTranslation()
-
-
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	scriptCategory = _("Unspoken")
 
 	def __init__(self, *args, **kwargs):
-		super(GlobalPlugin, self).__init__(*args, **kwargs)
-		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(
-			UnspokenSettingsPanel)
-
+		globalPluginHandler.GlobalPlugin.__init__(self, *args, **kwargs)
 		config.conf.spec["unspokenpy3"] = confspec
 
 		init_camlorn_audio()
-		loadSoundTheme(config.conf.spec["unspokenpy3"]["soundtheme"])
+		for key in sound_files:
+			sounds[key] = Sound3D(os.path.join(UNSPOKEN_SOUNDS_PATH, sound_files[key]))
+			sounds[key].set_rolloff_factor(0)
 		self._room_reverb = Reverb()
 		self._room_reverb.set_reverb_density(0)
 		self._room_reverb.set_Decay_time(0.4)
@@ -53,11 +88,12 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 		self._previous_mouse_object = None
 
+
 	def _hook_getSpeechTextForProperties(self, reason=NVDAObjects.controlTypes.REASON_QUERY, *args, **kwargs):
 		role = kwargs.get('role', None)
 		if role:
 			if config.conf["unspokenpy3"]["active"] and \
-					'role' in kwargs and role in sounds:
+				'role' in kwargs and role in sounds:
 				del kwargs['role']
 		return self._NVDA_getSpeechTextForProperties(reason, *args, **kwargs)
 
@@ -79,10 +115,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			else:
 				obj_x = desktop_max_x / 2.0
 				obj_y = desktop_max_y / 2.0
-			position_x = (obj_x / desktop_max_x) * \
-				(AUDIO_WIDTH * 2) - AUDIO_WIDTH
-			position_y = (obj_y / desktop_max_y) * (desktop_aspect *
-													AUDIO_WIDTH * 2) - (desktop_aspect * AUDIO_WIDTH)
+			position_x = (obj_x / desktop_max_x) * (AUDIO_WIDTH * 2) - AUDIO_WIDTH
+			position_y = (obj_y / desktop_max_y) * (desktop_aspect * AUDIO_WIDTH * 2) - (desktop_aspect * AUDIO_WIDTH)
 			position_y *= -1
 			sounds[role].set_position(position_x, position_y, AUDIO_DEPTH * -1)
 			sounds[role].play()
@@ -90,8 +124,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 	def event_becomeNavigatorObject(self, obj, nextHandler, isFocus=False):
 		if config.conf["unspokenpy3"]["active"]:
 			self.play_object(obj)
-		else:
-			pass
+		else: pass
 		nextHandler()
 
 	def event_mouseMove(self, obj, nextHandler, x, y):
@@ -106,20 +139,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			speech.cancelSpeech()
 			ui.message(_("Disable Unspoken"))
 			config.conf["unspokenpy3"]["active"] = False
-		elif config.conf["unspokenpy3"]["active"] == False:
+		elif  config.conf["unspokenpy3"]["active"] == False:
 			speech.cancelSpeech()
 			ui.message(_("Enable Unspoken"))
 			config.conf["unspokenpy3"]["active"] = True
-		else:
-			pass
+		else: pass
 
-	script_changeActivate.__doc__ = _(
-		"Changes the active  / deactive  mode of Unspoken.")
+	script_changeActivate.__doc__ = _("Changes the active  / deactive  mode of Unspoken.")
 	__gestures = {
 		"kb:control+shift+u": "changeActivate",
-	}
-
-	def terminate(self, *args, **kwargs):
-		super().terminate(*args, **kwargs)
-		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(
-			UnspokenSettingsPanel)
+		}
